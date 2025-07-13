a8dc7405ac5ef13ec4c951becb24475aab86dbab
XandraAd <ewuramaaddo@gmail.com>
2025-07-13 10:15:40 +0000

    create .env file

--- src/admin/Config/Firebase.js
+++ src/admin/Config/Firebase.js
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC72HTUVTxJJQJln4F9T4025DO0gnU2Zyo",
-  authDomain: "g-client-app.firebaseapp.com",
-  projectId: "g-client-app",
-  storageBucket: "g-client-app.firebasestorage.app",
-  messagingSenderId: "439925901384",
-  appId: "1:439925901384:web:23992cb569ff644dd8b07d",
-  measurementId: "G-0CMLK2CDEG",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId:import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
